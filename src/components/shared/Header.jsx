df584148bd58b5fe7571d3333c3e7bfcab61858a
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-04-28 20:09:49 +0600

    project setup

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Header = () => {
+  return (
+    <div>
+      <h2>This is Header Components</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2>This is Navbar Components</h2>
+    </div>
+  );
+};
+
+export default Navbar;
